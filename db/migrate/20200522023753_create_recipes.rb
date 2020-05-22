bbd381f7f57c8748e04b84f7b0d91aa6697b948f
German Cruz <cruzgerman216@yahoo.com>
2020-05-22 02:38:43 +0000

    Automatically backed up by Learn

--- db/migrate/20200522023753_create_recipes.rb
+++ db/migrate/20200522023753_create_recipes.rb
@@ -1,8 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :name 
-      t.string :ingredients 
+      t.string :name
+      t.string :ingredients
       t.string :cook_time
     end
   end
