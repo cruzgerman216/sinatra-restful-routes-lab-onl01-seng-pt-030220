b6887a787b918b6dd5f57110864d09afa97ca5e2
German Cruz <cruzgerman216@yahoo.com>
2020-05-22 05:15:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,4 +32,9 @@ class ApplicationController < Sinatra::Base
     redirect "/recipes/#{recipe.id}"
   end
 
+  patch 'recipes/:id' do
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  end
+
 end
