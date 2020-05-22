cc39d48d19069ed40e4891d65fabbbe094f08eb7
German Cruz <cruzgerman216@yahoo.com>
2020-05-22 05:30:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < Sinatra::Base
     puts "TEEEEEEEEST"
     puts recipe
     recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    recipe.save
     redirect "/recipes/#{recipe.id}"
   end
 
