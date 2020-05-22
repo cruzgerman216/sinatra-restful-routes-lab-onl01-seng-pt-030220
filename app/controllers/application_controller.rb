07ed0d9afcda06a80298d85c7c61e6916d1f3444
German Cruz <cruzgerman216@yahoo.com>
2020-05-22 05:30:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
 
   patch 'recipes/:id' do
     binding.pry
-    recipe = Recipe.find(params[:id])
+    recipe = Recipe.find_by_id(params[:id])
     puts "TEEEEEEEEST"
     puts recipe
     recipe.update(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
