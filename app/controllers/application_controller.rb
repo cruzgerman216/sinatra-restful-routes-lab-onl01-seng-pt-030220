34f036c44071005358270cdf86d448d4ed3ca9d8
German Cruz <cruzgerman216@yahoo.com>
2020-05-22 04:55:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,17 +10,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
-    erb :show
-  end
-
-  get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by_id(params[:id])
-
-  erb :edit
-  end
-
   get '/recipes/new' do
     erb :new
   end
